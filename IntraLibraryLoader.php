31d1ac72bc24daeaa290fc367ec8a30f3cab8ebd
Janek Lasocki-Biczysko <j.lasocki-biczysko@intrallect.com>
2012-09-11 12:57:53 +0100

    updating loader for better OO design

--- IntraLibraryLoader.php
+++ IntraLibraryLoader.php
@@ -1,15 +1,15 @@
-<?php 
+<?php
 /**
  * Class loader for IntraLibrary-PHP
- * 
+ *
  * @package IntraLibrary_PHP
  * @author  Janek Lasocki-Biczysko, <j.lasocki-biczysko@intrallect.com>
  *
  */
 class IntraLibraryLoader
 {
-	private static $CLASSES = array();
-	
+	private static $_instance;
+
 	/**
 	 * Register the loader SPL autoloader stack
 	 *
@@ -17,103 +17,111 @@ class IntraLibraryLoader
 	 */
 	public static function registerAutoloader()
 	{
-		self::$CLASSES = self::getClasses();
-		spl_autoload_register(__CLASS__ . '::load');
+		spl_autoload_register(array(self::getInstance(), 'load'));
 	}
-	
+
+	/**
+	 * Get the main instance of the autoloader
+	 *
+	 * @return IntraLibraryLoader
+	 */
+	public static function getInstance()
+	{
+		return self::$_instance ? self::$_instance : (self::$_instance = new self(__DIR__));
+	}
+
+	private $classes;
+	private $baseDir;
+
+	/**
+	 * Create an IntraLibraryLoader
+	 *
+	 * @param strign $baseDir the directory that hosts PHP classes
+	 */
+	public function __construct($baseDir)
+	{
+		$this->baseDir = $baseDir;
+		$this->loadClasses();
+	}
+
 	/**
 	 * Classloader
-	 * 
+	 *
 	 * @param string $classname the classname to load
 	 * @return boolean FALSE if class was not loaded
 	 */
-	public static function load($classname)
+	public function load($classname)
 	{
-		if (empty(self::$CLASSES[$classname]) ||
-			!include(self::$CLASSES[$classname]))
+		if (empty($this->classes[$classname]) ||
+			!include($this->classes[$classname]))
 		{
 			return FALSE;
 		}
 	}
-	
+
 	/**
 	 * Get all PHP classes part of IntraLibrary
 	 *
-	 * @return array an associative array mapping classname => filename
+	 * @return void
 	 */
-	public static function getClasses()
+	public function loadClasses()
 	{
-		$classes 	= array();
-		
-		foreach (self::_getFiles(dirname(__FILE__)) as $file)
+		$this->classes = array();
+
+		foreach (self::_getFiles($this->baseDir) as $file)
 		{
 			$pathinfo = pathinfo($file);
 			if (isset($pathinfo['extension']) && $pathinfo['extension'] == 'php')
 			{
-				$classes[$pathinfo['filename']] = $file;
+				$this->classes[$pathinfo['filename']] = $file;
 			}
 		}
-		
-		return $classes;
 	}
-	
+
 	/**
-	 * Get all files contained in this directory (looks recursively into subdirectories)
-	 * 
+	 * Get all classes found by this loader
+	 *
+	 * @return array an associative array mapping classname => filename
+	 */
+	public function getClasses()
+	{
+		if (empty($this->classes))
+		{
+			$this->loadClasses();
+		}
+
+		return $this->classes;
+	}
+
+	/**
+	 * Get all files contained in this directory, and it's subdirectories
+	 *
 	 * @param string $directory the directory to look into
 	 * @return array a list of files found
 	 */
-	private static function _getFiles($directory)
+	private function _getFiles($directory)
 	{
-		$directory 	= self::_sanitiseFilepath($directory);
+		$directory 	= rtrim($directory, '/') . '/';
 		$list 		= array();
-		
+
 		if ($handle = opendir($directory))
 		{
 			while (($file = readdir($handle)) !== FALSE)
 			{
 				if (is_dir($directory . $file) && $file != '.' && $file != '..')
 				{
-					$sublist = self::_getFiles($directory . $file);
+					$sublist = $this->_getFiles($directory . $file);
 					$list = array_merge($list, $sublist);
 				}
 				else if (is_file($directory . $file))
 				{
 					$list[] = $directory . $file;
 				}
-				
+
 			}
 			closedir($handle);
 		}
-		
+
 		return $list;
-	}
-	
-	/**
-	 * Santise a filepath
-	 * 
-	 * @param string  $path         the path to sanitise
-	 * @param boolean $append_slash if false won't append a slash
-	 * @return string
-	 */
-	private static function _sanitiseFilepath($path, $append_slash = TRUE)
-	{
-		// Convert to correct UNIX paths
-		$path = str_replace('\\', '/', $path);
-		$path = str_replace('../', '/', $path);
-		// replace // with / except when preceeded by :
-		$path = preg_replace("/([^:])\/\//", "$1/", $path);
-	
-		// Sort trailing slash
-		$path = trim($path);
-		// rtrim defaults plus /
-		$path = rtrim($path, " \n\t\0\x0B/");
-	
-		if ($append_slash)
-		{
-			$path = $path . '/';
-		}
-	
-		return $path;
 	}
 }
