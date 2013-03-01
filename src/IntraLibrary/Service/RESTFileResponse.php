8a38781e8effe4e76c5fd7b3216a43e602618468
Janek Lasocki-Biczysko <j.lasocki-biczysko@intrallect.com>
2013-03-01 11:08:51 +0000

    adding RESTFileResponse

---
+++ src/IntraLibrary/Service/RESTFileResponse.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace IntraLibrary\Service;
+
+/**
+ * A response object class for the IntraLibrary REST service
+ * expecting an IntraLibrary-REST endpoint file download response
+ *
+ * @package IntraLibrary_PHP
+ * @author  Janek Lasocki-Biczysko, <j.lasocki-biczysko@intrallect.com>
+ */
+class RESTFileResponse extends RESTResponse
+{
+	/**
+	 * Load the response data
+	 *
+	 * @param mixed $responseData the response data
+	 * @return void
+	 */
+	public function load($responseData)
+	{
+		// successful file download responses via CURL
+		// will return a TRUE
+		if ($responseData !== TRUE) {
+			parent::load($responseData);
+		}
+	}
+}
